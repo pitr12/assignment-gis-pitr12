8e2d084e525135a29c07899886610a389e45bd43
Peter Dubec <peter.dubec93@gmail.com>
2015-11-03 20:15:07 +0100

    removes hands-on folder

--- hands-on/session-1-homework.sql
+++
@@ -1,9 +0,0 @@
------------------- HOMEWORK--------------------------------
-------- 1 - How big is index? --------
-create index index_documents_on_published_on on documents(published_on);
-SELECT relname, (pg_relation_size(oid) / 1024 / 1024) AS "size in MB" FROM pg_class WHERE relname = 'index_documents_on_published_on';
-
-------2 --supplier index
-create index documents_supplier_index on documents(supplier)
-explain select * from documents where lower(supplier) = lower('SPP');
-create index document_lowercased_supplier on documents(lower(supplier))
