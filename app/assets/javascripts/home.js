4d22123755ea88caa641461cfa3051a4ac899a6b
Peter Dubec <peter.dubec93@gmail.com>
2015-11-04 20:50:56 +0100

    add default map to homepage

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+L.mapbox.accessToken = 'pk.eyJ1IjoicGl0cjEyIiwiYSI6ImNpZzN1a3ZmMDAybWx2bmtoZzdxcTY3ZTMifQ.MAq5KrbVv-505sbh9UzDQw';
+var map = L.mapbox.map('map', 'pitr12.ecdd20b0').setView([40, -74.50], 9);
+});
+
